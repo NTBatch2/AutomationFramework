0401e96f947267a91ad5e17ddb22e52fe1be9bd6
mercy <mercy@DESKTOP-GT9KGLF>
2020-05-17 14:34:57 -0400

    almost done the day

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 			System.out.println("Test");
 			System.out.println("test......");
+			System.out.println("WAWA");
 	}
 	//sara is adding comment
 	
