f022e4fa0319262cdc9664c85ecead79e2dcff69
bcade <bcade@LAPTOP-7OSUL94B.fios-router.home>
2020-05-17 14:22:05 -0400

    My try

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -4,10 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 			System.out.println("Test");
 			System.out.println("test......");
-			System.out.println("TEST...............");
-			
-			
-			System.out.println("Sara is testing for group 4 ....");
 	}
 	//sara is adding comment
 	//test
