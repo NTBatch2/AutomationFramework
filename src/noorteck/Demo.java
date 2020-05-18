a6e1c82a0afe8343909a2236c12de6b4d2548197
Uma Lubell <Uma Lubell@DESKTOP-O85TMHK>
2020-05-18 13:29:56 -0400

    created an object

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -1,43 +1,47 @@
 package noorteck;
 
 public class Demo {
-	
-	//instance variables
+
+	// instance variables
 	int num1 = 10;
 	int num2 = 15;
-	
+
 	public static void main(String[] args) {
-			System.out.println("Test");
-			System.out.println("test......");
+		System.out.println("Test");
+		System.out.println("test......");
+
+		// calling sum method
+		// creating an object
+		Demo obj = new Demo();
+		obj.sum(10, 15);
 	}
-	//sara is adding comment
-	
-	//andrea
-	
-	//adding method
+	// sara is adding comment
+
+	// andrea
+
+	// adding method
 	public int sum(int a, int b) {
 		return a + b;
 	}
-	
+
 	public void test1() {
 		System.out.println("andrea");
 		System.out.println("gitHub");
-		
+
 	}
-	
+
 	public void test() {
 		System.out.println("andrea");
-		
+
 		System.out.println("removed few lines");
-		
+
 	}
-	
+
 	// SABOOR
 	public void print() {
-	System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 	}
-	
+
 	// Andrea
 
-	
 }
