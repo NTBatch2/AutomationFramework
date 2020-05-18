5c6989d1813e9fefc69e651935dd508f049adbe1
Vio <Vio@Vio-PC.fios-router.home>
2020-05-17 22:14:37 -0400

    last commit for this Sunday

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -2,16 +2,23 @@ package noorteck;
 
 public class Demo {
 	public static void main(String[] args) {
+		
 			System.out.println("Test");
+			
 			System.out.println("test......");
 			
 			System.out.println("Denis");
+			
 			System.out.println("Dorina");
 			
 			System.out.println("Vio's TEST...for group two only..");
+			
 			System.out.println("Zahra");
+			
 			System.out.println("group 2");
 			
+			// Comment from VIO
+			System.out.println("Anything new today? ");
 	}
 	
 	
