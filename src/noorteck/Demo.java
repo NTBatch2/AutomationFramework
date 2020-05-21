94475aaf76e17392d4df0330e571f0d759cf1508
Vio <Vio@Vio-PC.fios-router.home>
2020-05-21 15:17:17 -0400

    Please add methods to the existig code..Tnx

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -1,23 +1,38 @@
 package noorteck;
 
+import java.util.HashMap;
+
 public class Demo {
+	
 	public static void main(String[] args) {
-			System.out.println("Test");
-			System.out.println("test......");
-			
-			System.out.println("Denis");
-			System.out.println("Dorina");
-			
-			System.out.println("Vio's TEST...for group two only..");
-			
-			System.out.println(" Heloo !");
-			System.out.println("WAWA");
-			System.out.println("Ali");
+		
+		System.out.println("Test");
+		System.out.println("test......");
+
+		System.out.println("Denis");
+		System.out.println("Dorina");
+
+		System.out.println("Vio's TEST...for group two only..");
+
+		System.out.println(" Heloo !");
+		System.out.println("WAWA");
+		System.out.println("Ali");
+
+		// Comment added by VIO,
+		HashMap<String, Boolean> map = new HashMap<>();
+
+		map.put("Roses", true);
+		map.put("Tulips", true);
+		map.put("Peonies", true);
+		map.put("Iris", false);
+		map.put(null, null);
+		map.put(null, false);
+
+		// Guys, please add methods to this HasMap..
+
 	}
-	
-	
-	
-	//sara is adding comment
-	
-	//Branching ss
+
+	// sara is adding comment
+
+	// Branching ss
 }
